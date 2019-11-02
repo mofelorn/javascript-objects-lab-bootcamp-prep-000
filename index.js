09350d31a2687e722114864ecc4503e919cb7d96
Felix Moajdo Jr <mojadof@yahoo.com>
2019-11-02 05:57:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,9 +12,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 
-function deleteFromObjectByKey(object, key) {
-  Object.assign({}, object, key)
-  delete object[key]
-  return object
-}
+
 
